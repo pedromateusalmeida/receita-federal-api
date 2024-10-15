18c51a1791d3f5d0f40183963cec0af3e7abd8a8
Pedro M Almeida <51126315+pedromateusalmeida@users.noreply.github.com>
2024-10-14 23:52:12 -0300

    feature selection, etl, testes

--- tratamento-dbt/receita_federal_tratamento/models/example/my_first_dbt_model.sql
+++ tratamento-dbt/receita_federal_tratamento/models/example/my_first_dbt_model.sql
@@ -10,10 +10,9 @@
 {{ config(materialized='table') }}
 
 WITH estabelecimentos AS (
-  
 SELECT
     e.*,
-    SPLIT(SPLIT(LOWER(e.EMAIL), '@')[SAFE_OFFSET(1)], '.')[SAFE_OFFSET(0)] AS PROVEDOR,
+    SPLIT(SPLIT(UPPER(e.EMAIL), '@')[SAFE_OFFSET(1)], '.')[SAFE_OFFSET(0)] AS PROVEDOR,
     LOWER(e.EMAIL) AS EMAIL_LOWER,
     m.MUNICIPIO AS MUNICIPIO_NOME,
     p.NM_PAIS,
@@ -27,28 +26,28 @@ SELECT
       WHEN SIT_CADASTRAL = 8 THEN 'BAIXADA'
       ELSE 'OUTRO'
     END AS NM_SIT_CADASTRAL,
-    FORMAT_DATE('%Y-%m-%d', SAFE.PARSE_DATE('%Y%m%d', DT_SIT_CADASTRAL)) AS DT_SIT_CADASTRAL_FORMATADA,
-    FORMAT_DATE('%Y-%m-%d', SAFE.PARSE_DATE('%Y%m%d', DT_INICIO_ATIVIDADE)) AS DT_INICIO_ATIVIDADE_FORMATADA
-  FROM
+    SAFE.PARSE_DATE('%Y%m%d', e.DT_SIT_CADASTRAL) AS DT_SIT_CADASTRAL_DATE,
+    SAFE.PARSE_DATE('%Y%m%d', e.DT_INICIO_ATIVIDADE) AS DT_INICIO_ATIVIDADE_DATE
+FROM
     `kinetic-valor-414812.receitafederal.estabelecimentos` e
-  LEFT JOIN
-    `kinetic-valor-414812.receitafederal.municipios` m
-    ON e.MUNICIPIO = m.ID_MUNICPIO
-  LEFT JOIN
-    `kinetic-valor-414812.receitafederal.paises` p
-    ON e.PAIS = p.COD_PAIS
-  LEFT JOIN
-    `kinetic-valor-414812.receitafederal.cnaes` c
-    ON e.CNAE_1 = c.COD_CNAE
-  LEFT JOIN
-    `kinetic-valor-414812.receitafederal.motivos` mo
-    ON e.MOTIVO_CADASTRAL = mo.COD_MOTIVO
+LEFT JOIN
+    `kinetic-valor-414812.receitafederal.municipios` m ON e.MUNICIPIO = m.ID_MUNICPIO
+LEFT JOIN
+    `kinetic-valor-414812.receitafederal.paises` p ON e.PAIS = p.COD_PAIS
+LEFT JOIN
+    `kinetic-valor-414812.receitafederal.cnaes` c ON e.CNAE_1 = c.COD_CNAE
+LEFT JOIN
+    `kinetic-valor-414812.receitafederal.motivos` mo ON e.MOTIVO_CADASTRAL = mo.COD_MOTIVO
 )
 SELECT
   *,
-  CONCAT(TIPO_LOUGRADOURO, ' ', LOGRADOURO, ', ', NUMERO, ' - ', MUNICIPIO_NOME, ', ', UF) AS ENDERECO_COMPLETO
+  CONCAT(TIPO_LOUGRADOURO, ' ', LOGRADOURO, ', ', NUMERO, ' - ', MUNICIPIO_NOME, ', ', UF) AS ENDERECO_COMPLETO,
+  EXTRACT(YEAR FROM DT_SIT_CADASTRAL_DATE) AS ANO_SIT_CADASTRAL,
+  EXTRACT(MONTH FROM DT_SIT_CADASTRAL_DATE) AS MES_SIT_CADASTRAL
 FROM
-  estabelecimentos
+  estabelecimentos es
+LEFT JOIN `kinetic-valor-414812.receitafederal.dicionario_provedores` dp ON es.PROVEDOR = dp.PROVEDOR_VALIDO
+
 
 /*
     Uncomment the line below to remove records with null `id` values
